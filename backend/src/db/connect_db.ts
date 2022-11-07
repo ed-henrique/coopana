9c3c2c868c9b1afde8576c6abd76f17d2c5b0410
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-11-07 12:01:17 -0400

    Now able to connect to DB
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/src/db/connect_db.ts
+++ backend/src/db/connect_db.ts
@@ -1,16 +1,20 @@
-require("dotenv").config();
-var mysql = require("mysql");
+import mysql from "mysql";
+import * as dotenv from "dotenv";
 
-var con = mysql.createConnection({
+dotenv.config();
+
+const connection = mysql.createConnection({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 });
 
-con.connect((err: any) => {
+connection.connect((err: any) => {
 	if (err) {
 		throw err;
 	}
 
 	console.log("CONNECTED!");
 });
+
+export default connection;
---
+++ backend/src/db/create_db.ts
@@ -0,0 +1,3 @@
+import connection from "./connect_db.js";
+
+console.log(connection);
